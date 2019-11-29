e223e860f6bf4f2ec25f053f85e7efffc0f91b89
Burak Köroğlu <burak20012009@gmail.com>
2019-11-29 22:13:34 +0300

    Update test.py

--- test.py
+++ test.py
@@ -11,8 +11,10 @@ f = open("result.txt", "w")
 def test_case_check((x1,y1),(x2,y2)):
     if x1 > x2:
         x1,x2 = x2,x1
+        test_case_check((x1,y1),(x2,y2))
     elif y1 > y2:
         y1,y2 = y2,y1
+        test_case_check((x1,y1),(x2,y2))
     elif  x1 == x2:
         x1 = randint(1,50)
         x2 = randint(1,50)
